6e654f98dc8f19dbde4473b6720fc4811395dde9
Sager Williams <sagerwilliams0011@gmail.com>
2020-02-06 14:33:50 -0600

    All components proxied

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 let app = express();
 
+app.use(cors());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
-let port = 1336;
+let port = 80;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
